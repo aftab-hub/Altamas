5433156f5d9b2b02e79e4dedcdd449d1c8b6ef8e
aftab-hub <aftabaftab1158@gmail.com>
2025-12-04 16:51:38 +0530

    updating url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectDB()
 await connectCloudinary()
 
 // Allow multiple origins
-const allowedOrigins = ['http://localhost:5173', '']
+const allowedOrigins = [process.env.FRONTEND_URL]
 
 app.post('/razorpay', razorpayWebhooks)
 
